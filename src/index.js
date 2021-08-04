1236a159c3dadbac7722b9df2d0467af510831e5
Vengord01 <64409652+Vengord01@users.noreply.github.com>
2021-08-04 15:48:18 +0300

    fix:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
+let a = [
+    "",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+];
+let b = [
+    "",
+    "",
+    "twenty ",
+    "thirty ",
+    "forty ",
+    "fifty ",
+    "sixty ",
+    "seventy ",
+    "eighty ",
+    "ninety ",
+];
+
+module.exports = function (number) {
+    const strNum = String(number);
+    if (number === 0) {
+        return "zero";
+    }
+    if (number < 20) {
+        return `${a[number]}`.trim();
   
-}
+    }
+    if (number >= 20 && number < 100) {
+        return `${b[strNum[0]]}${a[strNum[1]]}`.trim();
+  
+    }
+    if (
+        number >= 100 &&
+        +strNum.substring(1) >= 10 &&
+        +strNum.substring(1) <= 19
+   
+    ) {
+        return `${a[strNum[0]]}hundred ${a[+strNum.substring(1)]}`.trim();
+   
+    }
+    if (number >= 100) {
+        return `${a[strNum[0]]}hundred ${b[strNum[1]]}${a[strNum[2]]}`.trim();
+   
+    }
+};
